5126011503127d3df4d8c516a6635268f17db366
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-04-29 17:17:06 +0200

    Added 2 TCP projects for a "Simple Game App" - Learning a bit more about TCP protocol

---
+++ TcpGamesServer/Classes/Packet.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TcpGamesServer.Classes
+{
+    class Packet
+    {
+		// This decorators ensure correct serialization and deserialization of the properties with known Key names
+		[JsonProperty("command")]
+		public string Command { get; set; } = string.Empty;
+
+		[JsonProperty("message")]
+        public string Message { get; set; } = string.Empty; // Can be empty but not null
+
+		// Making a packet in the constructor
+		public Packet(string command, string message)
+		{
+			Command = command;
+			Message = message;
+		}
+
+		public override string ToString()
+		{
+			return $"[Packet:\n Command='{Command}'\n Message='{Message}']";
+		}
+
+		public string ToJson()
+		{
+			return JsonConvert.SerializeObject(this);
+		}
+
+		public static Packet FromJson(string jsonData)
+		{
+			if (string.IsNullOrEmpty(jsonData))
+			{
+				throw new ArgumentNullException(nameof(jsonData));
+			}
+			try
+			{
+				var packet = JsonConvert.DeserializeObject<Packet>(jsonData);
+				return packet ?? throw new InvalidOperationException("Deserialization returned null.");
+			}
+			catch (Exception ex)
+			{
+				throw new InvalidOperationException("Failed to deserialize JSON.", ex);
+			}
+		}
+	}
+}
